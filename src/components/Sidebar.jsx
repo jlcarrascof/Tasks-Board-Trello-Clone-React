e2e53784e213a2edbc0ecf88ec70eaf63fb9bfd5
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-10 19:14:55 -0400

    chore: Update Sidebar component width in Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className='bg-[#121417] h-[calc(100vh-3rem)] border-r border-r-[#9fadbc29] '>
+    <div className='bg-[#121417] h-[calc(100vh-3rem)] border-r border-r-[#9fadbc29] w-[280px] '>
       Sidebar
+      <div></div>
     </div>
   )
 }
