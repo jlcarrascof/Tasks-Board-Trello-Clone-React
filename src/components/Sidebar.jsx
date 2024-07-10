5584f91b4550a0e937ec99b92a1cd6386ea0a122
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-10 19:01:33 -0400

    chore: Update Sidebar component styles and height

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className='bg-[#121417]'>
+    <div className='bg-[#121417] h-[calc(100vh-3rem)] border-r border-r-[#9fadbc29] '>
       Sidebar
     </div>
   )
