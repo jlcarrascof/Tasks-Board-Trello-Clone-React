1a921e65952b86f0a3e65c87511dca5081a421c2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-10 19:18:37 -0400

    chore: Update Sidebar component to include a collapsible button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { ChevronRight } from 'react-feather'
 
 const Sidebar = () => {
   return (
     <div className='bg-[#121417] h-[calc(100vh-3rem)] border-r border-r-[#9fadbc29] w-[280px] '>
       Sidebar
-      <div></div>
+      <div>
+        <button>
+          <ChevronRight size={18} />
+        </button>
+      </div>
     </div>
   )
 }
