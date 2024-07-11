7a6331fabaff357bd9e3588a637a7c29aa641671
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-10 20:35:54 -0400

    chore: Update Sidebar component fixing styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,15 +5,15 @@ const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false)
 
   return (
-    <div className='bg-[#121417] h-[calc(100vh-3rem)] border-r border-r-[#9fadbc29] w-[280px] '>
+    <div className={`bg-[#121417] h-[calc(100vh-3rem)] border-r border-r-[#9fadbc29] ${collapsed ? 'w-[42px]' : 'w-[280px]'}`}>
       {collapsed && <div className='p-2'>
-        <button className='hover:bg-slate-600 rounded-sm'>
+        <button onClick={() => setCollapsed(!collapsed)}  className='hover:bg-slate-600 rounded-sm'>
           <ChevronRight size={18} />
         </button>
       </div>}
       {!collapsed && <div>
         Sidebar
-        <button className='hover:bg-slate-600 rounded-sm'>
+        <button onClick={() => setCollapsed(!collapsed)} className='hover:bg-slate-600 rounded-sm'>
           <ChevronLeft size={18} />
         </button>
       </div>}
